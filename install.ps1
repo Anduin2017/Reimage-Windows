750012af8b7fbfab1ff351d860b33934532d5d58
Anduin Xue <xuef@microsoft.com>
2021-06-26 22:39:02 +0800

    Only install if not installed.

--- install.ps1
+++ install.ps1
@@ -7,6 +7,19 @@ function Get-IsElevated {
     { Write-Output $false }   
 }
 
+function Install-IfNotInstalled {
+    param (
+        [string]$package
+    )
+
+    if ($(winget list --id FastCopy).Contains("-------------------------")) { 
+        Write-Host "$package is already installed!" -ForegroundColor Green
+    }
+    else {
+        winget install $package --id $package --scope machine -i --locale en-US
+    }
+}
+
 if (-not(Get-IsElevated)) { 
     throw "Please run this script as an administrator" 
 }
@@ -16,53 +29,53 @@ if (-not $(Get-Command winget)) {
     return
 }
 
-winget install Microsoft.VisualStudioCode
-winget install Microsoft.WindowsTerminal 
-winget install Microsoft.Teams 
-winget install Microsoft.Office
-winget install Microsoft.OneDrive
-winget install Microsoft.PowerShell
-winget install Microsoft.dotnet
-winget install Microsoft.Edge
-winget install Microsoft.Edge.Update
-winget install Microsoft.EdgeWebView2Runtime
+Install-IfNotInstalled Microsoft.VisualStudioCode
+Install-IfNotInstalled Microsoft.WindowsTerminal 
+Install-IfNotInstalled Microsoft.Teams 
+Install-IfNotInstalled Microsoft.Office
+Install-IfNotInstalled Microsoft.OneDrive
+Install-IfNotInstalled Microsoft.PowerShell
+Install-IfNotInstalled Microsoft.dotnet
+Install-IfNotInstalled Microsoft.Edge
+Install-IfNotInstalled Microsoft.Edge.Update
+Install-IfNotInstalled Microsoft.EdgeWebView2Runtime
 # We shall not install Visual Studio. Since the user may not buy enterprise license.
 # winget install Microsoft.VisualStudio.2019.Enterprise
-winget install Microsoft.AzureDataStudio
-winget install Microsoft.AzureStorageExplorer
-winget install Tencent.WeChat
-winget install FastCopy.FastCopy
-winget install SoftDeluxe.FreeDownloadManager
-winget install VideoLAN.VLC
-winget install OBSProject.OBSStudio
-winget install Git.git
-winget install OpenJS.NodeJS
-winget install Spotify.Spotify
-winget install Postman.Postman
+Install-IfNotInstalled Microsoft.AzureDataStudio
+Install-IfNotInstalled Microsoft.AzureStorageExplorer
+Install-IfNotInstalled Tencent.WeChat
+Install-IfNotInstalled FastCopy.FastCopy
+Install-IfNotInstalled SoftDeluxe.FreeDownloadManager
+Install-IfNotInstalled VideoLAN.VLC
+Install-IfNotInstalled OBSProject.OBSStudio
+Install-IfNotInstalled Git.git
+Install-IfNotInstalled OpenJS.NodeJS
+Install-IfNotInstalled Spotify.Spotify
+Install-IfNotInstalled Postman.Postman
 
-# Allow powershell script running. (Unsafe for some users.)
+Get-ChildItem -PathWrite-Host "Setting execution policy to remotesigned..." -ForegroundColor Yellow
 Set-ExecutionPolicy remotesigned
 
-# Set .NET environment
+Get-ChildItem -PathWrite-Host "Setting up .NET environment variables..." -ForegroundColor Yellow
 $env:ASPNETCORE_ENVIRONMENT = 'Development'
 $env:DOTNET_PRINT_TELEMETRY_MESSAGE = 'false'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
-# Copy back SSH keys. (Not available for all users. Copying back from my own OneDrive.)
-Get-ChildItem -Path $HOME | Where-Object { $_.Name -like "OneDrive*" }
+Get-ChildItem -PathWrite-Host "Copying back SSH keys..." -ForegroundColor Yellow
+$HOME | Where-Object { $_.Name -like "OneDrive*" }
 $OneDrivePath = $(Get-ChildItem -Path $HOME | Where-Object { $_.Name -like "OneDrive*" } | Sort-Object Name -Descending | Select-Object -First 1).Name
 Copy-Item -Path "$HOME\$OneDrivePath\Storage\SSH\*" -Destination "$HOME\.ssh\"
 
-# Copy back windows terminal configuration file. (Not available for all users. Copying back from my own OneDrive.)
+Write-Host "Copying back windows terminal configuration file..." -ForegroundColor Yellow
 $wtConfigPath = "C:\Users\xuef.FAREAST\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 Copy-Item -Path "$HOME\$OneDrivePath\Storage\WT\settings.json" -Destination $wtConfigPath
 
-# Config windows terminal context.
+Write-Host "Configuring windows terminal context menu..." -ForegroundColor Yellow
 git clone git@github.com:lextm/windowsterminal-shell.git "$HOME\temp"
 pwsh -command "$HOME\temp\install.ps1 mini"
 Remove-Item $HOME\temp -Force -Recurse -Confirm:$false
 
-# Test dev environment source code.
+Write-Host "Setting up .NET build environment..." -ForegroundColor Yellow
 Set-Location $HOME
 mkdir source
 mkdir source/repos
@@ -71,7 +84,7 @@ git clone git@github.com:AiursoftWeb/Infrastructures.git source/repos/AiursoftWe
 git clone git@github.com:AiursoftWeb/AiurVersionControl.git source/repos/AiursoftWeb
 dotnet test $HOME/source/repos/AiursoftWeb/
 
-# Show desktop icons
+Write-Host "Enabling desktop icons..." -ForegroundColor Yellow
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {59031a47-3f72-44a7-89c5-5595fe6b30ee} /t REG_DWORD /d 0 /f"
@@ -82,11 +95,13 @@ cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Hide
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {F02C1A0D-BE21-4350-88B0-7367FC96EF3C} /t REG_DWORD /d 0 /f"
 
 # Clean desktop
+Write-Host "Cleaning desktop..." -ForegroundColor Yellow
 Remove-Item $HOME\Desktop\* -Force -Recurse -Confirm:$false
 Remove-Item "C:\Users\Public\Desktop\*" -Force -Recurse -Confirm:$false
 Stop-Process -Name explorer -Force
 
 # Finally, upgrade all.
+Write-Host "Checking for final upgrades..." -ForegroundColor Yellow
 winget upgrade --all
 
 # Consider to reboot.
