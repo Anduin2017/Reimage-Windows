b14a9f271a62ed846ff1dd7f27b5fadda25c8d64
Anduin Xue <anduin@aiursoft.com>
2021-08-06 20:10:47 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -16,7 +16,7 @@ function Install-IfNotInstalled {
         Write-Host "$package is already installed!" -ForegroundColor Green
     }
     else {
-        Write-Host "Attempting to install: $package..." -ForegroundColor Yellow
+        Write-Host "Attempting to install: $package..." -ForegroundColor Green
         winget install $package
     }
 }
@@ -28,20 +28,20 @@ if (-not(Get-IsElevated)) {
 $computerName = Read-Host "Enter New Computer Name if you want to rename it: ($($env:COMPUTERNAME))"
 if (-not ([string]::IsNullOrEmpty($computerName)))
 {
-    Write-Host "Renaming computer to $computerName..." -ForegroundColor Yellow
+    Write-Host "Renaming computer to $computerName..." -ForegroundColor Green
     cmd /c "bcdedit /set {current} description `"$computerName`""
     Rename-Computer -NewName $computerName
 }
 
 if (-not $(Get-Command winget)) {
-    Write-Host "Installing WinGet..." -ForegroundColor Yellow
+    Write-Host "Installing WinGet..." -ForegroundColor Green
     $releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $releases = Invoke-RestMethod -uri "$($releases_url)"
     $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith("msixbundle") } | Select -First 1
     Invoke-WebRequest -Uri $latestRelease.browser_download_url -OutFile "C:\winget.msixbundle"
     Add-AppxPackage -Path .\winget.msixbundle
-    Write-Host "Reloading environment variables..." -ForegroundColor Yellow
+    Write-Host "Reloading environment variables..." -ForegroundColor Green
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
     if (-not $(Get-Command winget)) {
         start "C:\winget.msixbundle"
@@ -50,7 +50,7 @@ if (-not $(Get-Command winget)) {
             if (-not $(Get-Command winget))
             {
                 Write-Host "Winget is still not found!" -ForegroundColor Yellow
-                Start-Sleep -Milliseconds 500
+                Start-Sleep -Milliseconds 5000
             } 
             else
             {
@@ -60,6 +60,12 @@ if (-not $(Get-Command winget)) {
     }
     Remove-Item -Path "C:\winget.msixbundle" -Force
 }
+
+Write-Host "Triggering Store to upgrade all apps..." -ForegroundColor Green
+$namespaceName = "root\cimv2\mdm\dmmap"
+$className = "MDM_EnterpriseModernAppManagement_AppManagement01"
+$wmiObj = Get-WmiObject -Namespace $namespaceName -Class $className
+$result = $wmiObj.UpdateScanMethod()
 
 #apps want to install
 $appList = @(
@@ -89,11 +95,11 @@ foreach ($app in $appList){
     Install-IfNotInstalled $app
 }
 
-Write-Host "Reloading environment variables..." -ForegroundColor Yellow
+Write-Host "Reloading environment variables..." -ForegroundColor Green
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
  
 if (-not $env:Path.Contains("mpeg") -or -not $(Get-Command ffmpeg)) {
-    Write-Host "Downloading FFmpeg..." -ForegroundColor Yellow
+    Write-Host "Downloading FFmpeg..." -ForegroundColor Green
     $ffmpegPath = "C:\Program Files\FFMPEG"
     $downloadUri = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z"
     Invoke-WebRequest -Uri $downloadUri -OutFile "C:\ffmpeg.7z"
@@ -101,7 +107,7 @@ if (-not $env:Path.Contains("mpeg") -or -not $(Get-Command ffmpeg)) {
     $subPath = $(Get-ChildItem -Path $ffmpegPath | Where-Object { $_.Name -like "ffmpeg*" } | Sort-Object Name -Descending | Select-Object -First 1).Name
     $subPath = Join-Path -Path $ffmpegPath -ChildPath $subPath
     $binPath = Join-Path -Path $subPath -ChildPath "bin"
-    Write-Host "Adding FFmpeg to PATH..." -ForegroundColor Yellow
+    Write-Host "Adding FFmpeg to PATH..." -ForegroundColor Green
     [Environment]::SetEnvironmentVariable(
         "Path",
         [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";$binPath",
@@ -115,45 +121,45 @@ Write-Host "Disable Sleep on AC Power..." -ForegroundColor Green
 Powercfg /Change monitor-timeout-ac 20
 Powercfg /Change standby-timeout-ac 0
 
-Write-Host "Setting up some node js global tools..." -ForegroundColor Yellow
+Write-Host "Setting up some node js global tools..." -ForegroundColor Green
 npm install --global npm@latest
 npm install --global node-static typescript @angular/cli yarn
 
-Write-Host "Setting execution policy to remotesigned..." -ForegroundColor Yellow
+Write-Host "Setting execution policy to remotesigned..." -ForegroundColor Green
 Set-ExecutionPolicy remotesigned
 
-Write-Host "Setting up .NET environment variables..." -ForegroundColor Yellow
+Write-Host "Setting up .NET environment variables..." -ForegroundColor Green
 [Environment]::SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development", "Machine")
 [Environment]::SetEnvironmentVariable("DOTNET_PRINT_TELEMETRY_MESSAGE", "false", "Machine")
 [Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1", "Machine")
 
-Write-Host "Copying back SSH keys..." -ForegroundColor Yellow
+Write-Host "Copying back SSH keys..." -ForegroundColor Green
 $OneDrivePath = $(Get-ChildItem -Path $HOME | Where-Object { $_.Name -like "OneDrive*" } | Sort-Object Name -Descending | Select-Object -First 1).Name
 mkdir $HOME\.ssh -Force
 Copy-Item -Path "$HOME\$OneDrivePath\Storage\SSH\*" -Destination "$HOME\.ssh\"
 
-Write-Host "Configuring git..." -ForegroundColor Yellow
+Write-Host "Configuring git..." -ForegroundColor Green
 git config --global user.email "anduin.xue@microsoft.com"
 git config --global user.name "Anduin Xue"
 git config --global core.autocrlf true
 
-Write-Host "Copying back windows terminal configuration file..." -ForegroundColor Yellow
+Write-Host "Copying back windows terminal configuration file..." -ForegroundColor Green
 $wtConfigPath = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 Copy-Item -Path "$HOME\$OneDrivePath\Storage\WT\settings.json" -Destination $wtConfigPath
 
-Write-Host "Configuring windows terminal context menu..." -ForegroundColor Yellow
+Write-Host "Configuring windows terminal context menu..." -ForegroundColor Green
 git clone https://github.com/lextm/windowsterminal-shell.git "$HOME\temp"
 pwsh -command "$HOME\temp\install.ps1 mini"
 Remove-Item $HOME\temp -Force -Recurse -Confirm:$false
 
-Write-Host "Setting up .NET build environment..." -ForegroundColor Yellow
+Write-Host "Setting up .NET build environment..." -ForegroundColor Green
 dotnet tool install --global dotnet-ef
 dotnet tool update --global dotnet-ef
 git clone https://github.com/AiursoftWeb/Infrastructures.git "$HOME/source/repos/AiursoftWeb/Infrastructures"
 git clone https://github.com/AiursoftWeb/AiurVersionControl.git "$HOME/source/repos/AiursoftWeb/AiurVersionControl"
 dotnet test "$HOME\source\repos\AiursoftWeb\Infrastructures\Aiursoft.Infrastructures.sln"
 
-Write-Host "Enabling desktop icons..." -ForegroundColor Yellow
+Write-Host "Enabling desktop icons..." -ForegroundColor Green
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {59031a47-3f72-44a7-89c5-5595fe6b30ee} /t REG_DWORD /d 0 /f"
@@ -168,37 +174,37 @@ cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Hide
 # Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name "UserAuthentication" -Value 1
 # Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
-Write-Host "Enabling Chinese input method..." -ForegroundColor Yellow
+Write-Host "Enabling Chinese input method..." -ForegroundColor Green
 $LanguageList = Get-WinUserLanguageList
 $LanguageList.Add("zh-CN")
 Set-WinUserLanguageList $LanguageList -Force
 
-Write-Host "Installing Github.com/microsoft/artifacts-credprovider..." -ForegroundColor Yellow
+Write-Host "Installing Github.com/microsoft/artifacts-credprovider..." -ForegroundColor Green
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/microsoft/artifacts-credprovider/master/helpers/installcredprovider.ps1'))
 
-Write-Host "Removing Bluetooth icons..." -ForegroundColor Yellow
+Write-Host "Removing Bluetooth icons..." -ForegroundColor Green
 cmd.exe /c "reg add `"HKCU\Control Panel\Bluetooth`" /v `"Notification Area Icon`" /t REG_DWORD /d 0 /f"
 
-Write-Host "Disabling apps auto start..." -ForegroundColor Yellow
+Write-Host "Disabling apps auto start..." -ForegroundColor Green
 cmd.exe /c "reg delete  HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v Wechat /f"
 cmd.exe /c "reg delete  HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v `"Free Download Manager`" /f"
 
-Write-Host "Applying file explorer settings..." -ForegroundColor Yellow
+Write-Host "Applying file explorer settings..." -ForegroundColor Green
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v AutoCheckSelect /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v LaunchTo /t REG_DWORD /d 1 /f"
 
-Write-Host "Setting Time zone..." -ForegroundColor Yellow
+Write-Host "Setting Time zone..." -ForegroundColor Green
 Set-TimeZone -Name "China Standard Time"
 
-Write-Host "Setting mouse speed..." -ForegroundColor Yellow
+Write-Host "Setting mouse speed..." -ForegroundColor Green
 cmd.exe /c "reg add `"HKCU\Control Panel\Mouse`" /v MouseSensitivity /t REG_SZ /d 6 /f"
 cmd.exe /c "reg add `"HKCU\Control Panel\Mouse`" /v MouseSpeed /t REG_SZ /d 0 /f"
 cmd.exe /c "reg add `"HKCU\Control Panel\Mouse`" /v MouseThreshold1 /t REG_SZ /d 0 /f"
 cmd.exe /c "reg add `"HKCU\Control Panel\Mouse`" /v MouseThreshold2 /t REG_SZ /d 0 /f"
 
 # Clean desktop
-Write-Host "Cleaning desktop..." -ForegroundColor Yellow
+Write-Host "Cleaning desktop..." -ForegroundColor Green
 Remove-Item $HOME\Desktop\* -Force -Recurse -Confirm:$false
 Remove-Item "C:\Users\Public\Desktop\*" -Force -Recurse -Confirm:$false
 Stop-Process -Name explorer -Force
@@ -208,13 +214,13 @@ if ("$(winget list --id Spotify)".Contains("--")) {
     Write-Host "Spotify is already installed!" -ForegroundColor Green
 }
 else {
-    Write-Host "Attempting to download spotify installer..." -ForegroundColor Yellow
+    Write-Host "Attempting to download spotify installer..." -ForegroundColor Green
     $source = 'https://download.scdn.co/SpotifySetup.exe'
     Invoke-WebRequest -Uri $source -OutFile "$HOME\Desktop\spotify.exe"
 }
 
 # Upgrade all.
-Write-Host "Checking for final upgrades..." -ForegroundColor Yellow
+Write-Host "Checking for final app upgrades..." -ForegroundColor Green
 winget upgrade --all
 
 # Do some cleaning.
