634ad746fbeb7bb03dcca1f7865ff8342eef2d5f
Anduin Xue <anduin@aiursoft.com>
2021-09-02 02:54:46 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -21,6 +21,10 @@ function Install-IfNotInstalled {
     }
 }
 
+Write-Host "-----------------------------" -ForegroundColor Green
+Write-Host "        PART 1  - Prepare    " -ForegroundColor Green
+Write-Host "-----------------------------" -ForegroundColor Green
+
 if (-not(Get-IsElevated)) { 
     throw "Please run this script as an administrator" 
 }
@@ -43,14 +47,6 @@ if (-not ([string]::IsNullOrEmpty($computerName)))
 
 if (-not $(Get-Command winget)) {
      Write-Host "Installing WinGet..." -ForegroundColor Green
-#    $releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-#    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#    $releases = Invoke-RestMethod -uri "$($releases_url)"
-#    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith("msixbundle") } | Select -First 1
-#    Invoke-WebRequest -Uri $latestRelease.browser_download_url -OutFile "C:\winget.msixbundle"
-#    Add-AppxPackage -Path .\winget.msixbundle
-#    Write-Host "Reloading environment variables..." -ForegroundColor Green
-#    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
     start "ms-appinstaller:?source=https://aka.ms/getwinget"
     if (-not $(Get-Command winget)) {
         start "C:\winget.msixbundle"
@@ -69,6 +65,10 @@ if (-not $(Get-Command winget)) {
     }
     Remove-Item -Path "C:\winget.msixbundle" -Force
 }
+
+Write-Host "-----------------------------" -ForegroundColor Green
+Write-Host "        PART 2  - Install    " -ForegroundColor Green
+Write-Host "-----------------------------" -ForegroundColor Green
 
 Write-Host "Triggering Store to upgrade all apps..." -ForegroundColor Green
 $namespaceName = "root\cimv2\mdm\dmmap"
@@ -169,13 +169,16 @@ else {
     winget install --exact --id Microsoft.VisualStudioCode --scope Machine --interactive
 }
 
+Write-Host "-----------------------------" -ForegroundColor Green
+Write-Host "        PART 3  - Terminal    " -ForegroundColor Green
+Write-Host "-----------------------------" -ForegroundColor Green
+
 Write-Host "Enabling OneDrive silent sign in..." -ForegroundColor Green
 $HKLMregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive'##Path to HKLM keys
 $DiskSizeregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\DiskSpaceCheckThresholdMB'##Path to max disk size key
-
 if(!(Test-Path $HKLMregistryPath)){New-Item -Path $HKLMregistryPath -Force}
 if(!(Test-Path $DiskSizeregistryPath)){New-Item -Path $DiskSizeregistryPath -Force}
-Write-Host "Tenant Id is $($aad.TenantId)"
+Write-Host "Current AAD Tenant Id is $($aad.TenantId)"
 New-ItemProperty -Path $HKLMregistryPath -Name 'SilentAccountConfig' -Value '1' -PropertyType DWORD -Force | Out-Null ##Enable silent account configuration
 New-ItemProperty -Path $DiskSizeregistryPath -Name $aad.TenantId -Value '102400' -PropertyType DWORD -Force | Out-Null ##Set max OneDrive threshold before prompting
 Write-Host "Restarting OneDrive..." -ForegroundColor Yellow
@@ -184,14 +187,6 @@ explorer "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
 Start-Sleep -Seconds 10
 $OneDrivePath = $(Get-ChildItem -Path $HOME | Where-Object { $_.Name -like "OneDrive*" } | Sort-Object Name -Descending | Select-Object -First 1).Name
 Get-ChildItem $OneDrivePath
-
-Write-Host "Disable Sleep on AC Power..." -ForegroundColor Green
-Powercfg /Change monitor-timeout-ac 20
-Powercfg /Change standby-timeout-ac 0
-
-Write-Host "Setting up some node js global tools..." -ForegroundColor Green
-npm install --global npm@latest
-npm install --global node-static typescript @angular/cli yarn
 
 Write-Host "Setting execution policy to remotesigned..." -ForegroundColor Green
 Set-ExecutionPolicy remotesigned
@@ -206,8 +201,6 @@ Set-Content $PROFILE "function Update-All {
     iex ((New-Object System.Net.WebClient).DownloadString('https://github.com/Anduin2017/configuration-script-win/raw/main/install.ps1'))
 }"
 . $PROFILE
-
-
 
 Write-Host "Copying back SSH keys..." -ForegroundColor Green
 mkdir $HOME\.ssh -ErrorAction SilentlyContinue
@@ -231,6 +224,14 @@ git clone https://github.com/lextm/windowsterminal-shell.git "$HOME\temp"
 pwsh -command "$HOME\temp\install.ps1 mini"
 Remove-Item $HOME\temp -Force -Recurse -Confirm:$false
 
+Write-Host "-----------------------------" -ForegroundColor Green
+Write-Host "        PART 4  - SDK    " -ForegroundColor Green
+Write-Host "-----------------------------" -ForegroundColor Green
+
+Write-Host "Setting up some node js global tools..." -ForegroundColor Green
+npm install --global npm@latest
+npm install --global node-static typescript @angular/cli yarn
+
 Write-Host "Setting up .NET environment variables..." -ForegroundColor Green
 [Environment]::SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development", "Machine")
 [Environment]::SetEnvironmentVariable("DOTNET_PRINT_TELEMETRY_MESSAGE", "false", "Machine")
@@ -246,7 +247,11 @@ Write-Host "Building some .NET projects to ensure you can develop..." -Foregroun
 git clone https://github.com/AiursoftWeb/Infrastructures.git "$HOME\source\repos\AiursoftWeb\Infrastructures"
 git clone https://github.com/AiursoftWeb/AiurVersionControl.git "$HOME\source\repos\AiursoftWeb\AiurVersionControl"
 git clone https://github.com/Anduin2017/Happiness-recorder.git "$HOME\source\repos\Anduin2017\Happiness-recorder"
-dotnet publish "$HOME\source\repos\AiursoftWeb\Happiness-recorder\JAI.csproj" -c Release -r win-x64 -o "$OneDrivePath\Storage\Tools\JAL"
+dotnet publish "$HOME\source\repos\Anduin2017\Happiness-recorder\JAI.csproj" -c Release -r win-x64 -o "$OneDrivePath\Storage\Tools\JAL"
+
+Write-Host "-----------------------------" -ForegroundColor Green
+Write-Host "        PART 5  - Desktop    " -ForegroundColor Green
+Write-Host "-----------------------------" -ForegroundColor Green
 
 Write-Host "Enabling desktop icons..." -ForegroundColor Green
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
@@ -258,21 +263,9 @@ cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Hide
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {F02C1A0D-BE21-4350-88B0-7367FC96EF3C} /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {F02C1A0D-BE21-4350-88B0-7367FC96EF3C} /t REG_DWORD /d 0 /f"
 
-Write-Host "Setting UAC..." -ForegroundColor Green
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" -Name "ConsentPromptBehaviorAdmin" -Value 5
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" -Name "PromptOnSecureDesktop" -Value 1
-
-Write-Host "Enable Remote Desktop..." -ForegroundColor Green
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\" -Name "fDenyTSConnections" -Value 0
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name "UserAuthentication" -Value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-
-$networkProfiles = Get-NetConnectionProfile
-foreach ($networkProfile in $networkProfiles) {
-    Write-Host "Setting network $($networkProfile.Name) to home network to enable more features..." -ForegroundColor Green
-    Write-Host "This is dangerous because your roommates may detect your device is online." -ForegroundColor Yellow
-    Set-NetConnectionProfile -Name $networkProfile.Name -NetworkCategory Private
-}
+Write-Host "Disable Sleep on AC Power..." -ForegroundColor Green
+Powercfg /Change monitor-timeout-ac 20
+Powercfg /Change standby-timeout-ac 0
 
 Write-Host "Enabling Chinese input method..." -ForegroundColor Green
 $LanguageList = Get-WinUserLanguageList
@@ -292,14 +285,15 @@ cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Adva
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v LaunchTo /t REG_DWORD /d 1 /f"
 
 Write-Host "Setting Time zone..." -ForegroundColor Green
-Write-Host "Time zone set to China Standard Time."
 Set-TimeZone -Name "China Standard Time"
+Write-Host "Time zone set to China Standard Time."
 
 Write-Host "Setting mouse speed..." -ForegroundColor Green
 cmd.exe /c "reg add `"HKCU\Control Panel\Mouse`" /v MouseSensitivity /t REG_SZ /d 6 /f"
 cmd.exe /c "reg add `"HKCU\Control Panel\Mouse`" /v MouseSpeed /t REG_SZ /d 0 /f"
 cmd.exe /c "reg add `"HKCU\Control Panel\Mouse`" /v MouseThreshold1 /t REG_SZ /d 0 /f"
 cmd.exe /c "reg add `"HKCU\Control Panel\Mouse`" /v MouseThreshold2 /t REG_SZ /d 0 /f"
+Write-Host "Mouse speed changed. Will apply next reboot." -ForegroundColor Yellow
 
 Write-Host "Pin repos to quick access..." -ForegroundColor Green
 $load_com = new-object -com shell.application
@@ -314,8 +308,30 @@ Write-Host "Dark theme enabled."
 Write-Host "Cleaning desktop..." -ForegroundColor Green
 Remove-Item $HOME\Desktop\* -Force -Recurse -Confirm:$false
 Remove-Item "C:\Users\Public\Desktop\*" -Force -Recurse -Confirm:$false
+
+Write-Host "Resetting desktop..." -ForegroundColor Yellow
 Stop-Process -Name explorer -Force
 Write-Host "Desktop cleaned."
+
+Write-Host "-----------------------------" -ForegroundColor Green
+Write-Host "        PART 6  - Security    " -ForegroundColor Green
+Write-Host "-----------------------------" -ForegroundColor Green
+
+$networkProfiles = Get-NetConnectionProfile
+foreach ($networkProfile in $networkProfiles) {
+    Write-Host "Setting network $($networkProfile.Name) to home network to enable more features..." -ForegroundColor Green
+    Write-Host "This is dangerous because your roommates may detect your device is online." -ForegroundColor Yellow
+    Set-NetConnectionProfile -Name $networkProfile.Name -NetworkCategory Private
+}
+
+Write-Host "Setting UAC..." -ForegroundColor Green
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" -Name "ConsentPromptBehaviorAdmin" -Value 5
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" -Name "PromptOnSecureDesktop" -Value 1
+
+Write-Host "Enable Remote Desktop..." -ForegroundColor Green
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\" -Name "fDenyTSConnections" -Value 0
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name "UserAuthentication" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 # Upgrade all.
 Write-Host "Checking for final app upgrades..." -ForegroundColor Green
