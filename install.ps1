a87dde596e42e5ead81ef1ab68f7560a6ff59334
Anduin Xue <anduin@aiursoft.com>
2021-09-16 13:04:57 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -276,6 +276,9 @@ Write-Host "-----------------------------" -ForegroundColor Green
 Write-Host "        PART 5  - Desktop    " -ForegroundColor Green
 Write-Host "-----------------------------" -ForegroundColor Green
 
+Write-Host "Clearing recycle bin..." -ForegroundColor Green
+Clear-RecycleBin -DriveLetter $driveLetter -Force -Confirm
+
 Write-Host "Enabling desktop icons..." -ForegroundColor Green
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
