8fb6d5136b9421e8530778729762e5bc3e910ed3
Anduin Xue <anduin@aiursoft.com>
2021-09-27 00:16:57 +0800

    Remove rubbish 3D objects.

--- install.ps1
+++ install.ps1
@@ -288,8 +288,8 @@ Write-Host "-----------------------------" -ForegroundColor Green
 Write-Host "Clearing recycle bin..." -ForegroundColor Green
 Clear-RecycleBin -DriveLetter $driveLetter -Force -Confirm
 
-Write-Host "Setting up zoom level..." -ForegroundColor Green
-
+Write-Host "Remove rubbish 3D objects..." -ForegroundColor Green
+Remove-Item 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}' -ErrorAction SilentlyContinue
 
 Write-Host "Enabling desktop icons..." -ForegroundColor Green
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
