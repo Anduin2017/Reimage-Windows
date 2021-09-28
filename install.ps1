d7d416ab185bafd048b3fff01973dc45a467ddcd
Anduin Xue <anduin@aiursoft.com>
2021-09-28 22:30:23 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -201,31 +201,15 @@ Get-ChildItem $OneDrivePath
 Write-Host "Setting execution policy to remotesigned..." -ForegroundColor Green
 Set-ExecutionPolicy remotesigned
 
-Write-Host "Installing this script as Update-All..." -ForegroundColor Green
+Write-Host "Installing profile file..." -ForegroundColor Green
 if (!(Test-Path $PROFILE))
 {
    Write-Host "Creating PROFILE..." -ForegroundColor Yellow
    New-Item -Path $PROFILE -ItemType "file" -Force
 }
-Set-Content $PROFILE "
 
-# PROFILE FILE
-Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
-function Update-All {
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://github.com/Anduin2017/configuration-script-win/raw/main/install.ps1'))
-}
-
-function Watch-RandomVideo {
-    `$allVideos = Get-ChildItem -Path . -Include ('*.wmv', '*.avi', '*.mp4') -Recurse -ErrorAction SilentlyContinue -Force
-
-    while (`$true) {
-        `$pickedVideo = `$(Get-Random -InputObject `$allVideos).FullName
-        Write-Host 'Picked to play `$pickedVideo' -ForegroundColor Yellow
-        Start-Process 'C:\Program Files\VideoLAN\VLC\vlc.exe' -PassThru '--start-time 9 `$pickedVideo' -Wait
-    }
-}
-
-"
+$profileContent = (New-Object System.Net.WebClient).DownloadString('https://github.com/Anduin2017/configuration-script-win/raw/main/PROFILE.ps1')
+Set-Content $PROFILE $profileContent
 . $PROFILE
 
 Write-Host "Linking back SSH keys..." -ForegroundColor Green
