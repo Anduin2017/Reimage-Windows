29f7d6fd6165a4a124c94d09fc132222ddf24fbc
Anduin Xue <anduin@aiursoft.com>
2021-09-28 22:27:26 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -219,7 +219,7 @@ function Watch-RandomVideo {
     `$allVideos = Get-ChildItem -Path . -Include ('*.wmv', '*.avi', '*.mp4') -Recurse -ErrorAction SilentlyContinue -Force
 
     while (`$true) {
-        `$pickedVideo = `$(Get-Random -InputObject $allVideos).FullName
+        `$pickedVideo = `$(Get-Random -InputObject `$allVideos).FullName
         Write-Host 'Picked to play `$pickedVideo' -ForegroundColor Yellow
         Start-Process 'C:\Program Files\VideoLAN\VLC\vlc.exe' -PassThru '--start-time 9 `$pickedVideo' -Wait
     }
