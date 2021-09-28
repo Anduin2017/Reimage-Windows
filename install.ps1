ae9f37fa14fe802547be0ced7912617ddf3a0370
Anduin Xue <anduin@aiursoft.com>
2021-09-28 22:25:00 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -208,10 +208,24 @@ if (!(Test-Path $PROFILE))
    New-Item -Path $PROFILE -ItemType "file" -Force
 }
 Set-Content $PROFILE "
+
+# PROFILE FILE
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
 function Update-All {
-    iex ((New-Object System.Net.WebClient).DownloadString('https://github.com/Anduin2017/configuration-script-win/raw/main/install.ps1'))
-}"
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://github.com/Anduin2017/configuration-script-win/raw/main/install.ps1'))
+}
+
+function Watch-RandomVideo {
+    $allVideos = Get-ChildItem -Path . -Include ('*.wmv', '*.avi', '*.mp4') -Recurse -ErrorAction SilentlyContinue -Force
+
+    while ($true) {
+        $pickedVideo = $(Get-Random -InputObject $allVideos).FullName
+        Write-Host 'Picked to play $pickedVideo' -ForegroundColor Yellow
+        Start-Process 'C:\Program Files\VideoLAN\VLC\vlc.exe' -PassThru "--start-time 9 $pickedVideo" -Wait
+    }
+}
+
+"
 . $PROFILE
 
 Write-Host "Linking back SSH keys..." -ForegroundColor Green
