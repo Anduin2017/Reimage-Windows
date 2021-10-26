112b5cf40f176263a92bff0ad01e8de47899f757
Anduin Xue <anduin@aiursoft.com>
2021-10-26 13:15:42 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -89,8 +89,10 @@ if (-not(Get-IsElevated)) {
     throw "Please run this script as an administrator" 
 }
 
-if (-not $(Get-Command Connect-AzureAD)) {
+if (-not $(Get-Command Connect-AzureAD -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing Nuget PowerShell Package Provider..." -ForegroundColor Green
     Install-PackageProvider -Name NuGet -Force
+    Write-Host "Installing AzureAD PowerShell module..." -ForegroundColor Green
     Install-Module AzureAD -Force
 } else {
     Write-Host "Azure AD PowerShell Module is already installed!" -ForegroundColor Green
