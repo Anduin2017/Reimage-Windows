db25beeeaf15ff1a3daffe858bdbc17981fba377
Anduin Xue <anduin@aiursoft.com>
2021-11-07 17:25:59 +0800

    Update PROFILE.ps1

--- PROFILE.ps1
+++ PROFILE.ps1
@@ -15,12 +15,17 @@ function Force-UpdateAll {
 function Watch-RandomVideo {
     param(
         [string]$filter,
+        [string]$exclude,
         [int]$take = 99999999
     )
 
     Write-Host "Fetching videos..."
     $allVideos = Get-ChildItem -Path . -Include ('*.wmv', '*.avi', '*.mp4') -Recurse -ErrorAction SilentlyContinue -Force
-    $allVideos = $allVideos | Sort-Object { Get-Random } | Where-Object { $_.VersionInfo.FileName.Contains($filter) } | Select-Object -First $take
+    $allVideos = $allVideos | Sort-Object { Get-Random } | Where-Object { $_.VersionInfo.FileName.Contains($filter) }
+    if (-not ([string]::IsNullOrEmpty($exclude))) {
+        $allVideos = $allVideos | Where-Object { -not $_.VersionInfo.FileName.Contains($exclude) }
+    }
+    $allVideos = $allVideos | Select-Object -First $take
     $allVideos | Format-Table -AutoSize | Select-Object -First 20
     Write-Host "Playing $($allVideos.Count) videos..."
     foreach ($pickedVideo in $allVideos) {
@@ -29,6 +34,14 @@ function Watch-RandomVideo {
         Write-Host "$pickedVideo" -ForegroundColor White
         Start-Sleep -Seconds 1
         Start-Process "C:\Program Files\VideoLAN\VLC\vlc.exe" -PassThru "--start-time 9 `"$pickedVideo`"" -Wait 2>&1 | out-null
+
+        $vote = Read-Host "How do you like that? (A-B-C-D E-F-G)"
+        if (-not ([string]::IsNullOrEmpty($vote))) {
+            $destination = "Sorted-Level-$vote"
+            Write-Host "Moving $pickedVideo to $destination..." -ForegroundColor Green
+            New-Item -Type "Directory" -Name $destination -ErrorAction SilentlyContinue
+            Move-Item -Path $pickedVideo -Destination $destination
+        }
     }
 }
 
@@ -50,4 +63,5 @@ function Watch-RandomPhoto {
         Start-Sleep -Seconds 4
     }
 }
+
 
