c36e323211bdae3baf6b01eb02e570f12112dd0e
Anduin Xue <Anduin@aiursoft.com>
2022-03-03 20:30:56 +0800

    Add restore photo viewer reg.

--- install.ps1
+++ install.ps1
@@ -484,6 +484,8 @@ $UserLanguageList | Format-Table -AutoSize
 Write-Host "Enabling Hardware-Accelerated GPU Scheduling" -ForegroundColor Green
 New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\" -Name 'HwSchMode' -Value '2' -PropertyType DWORD -Force
 
+
+
 Write-Host "Removing Bluetooth icons..." -ForegroundColor Green
 cmd.exe /c "reg add `"HKCU\Control Panel\Bluetooth`" /v `"Notification Area Icon`" /t REG_DWORD /d 0 /f"
 
