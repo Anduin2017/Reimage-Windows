8f7279c22794b2be6425d2080c538b60f5288690
Anduin Xue <anduin@aiursoft.com>
2022-05-07 18:44:03 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -617,14 +617,16 @@ if ($pressedKey -eq 'c') {
     Write-Host "Installing updates... (Computer will reboot in minutes...)" -ForegroundColor Green
     Get-WindowsUpdate -AcceptAll -Install -ForceInstall -AutoReboot
 
-    cmd.exe /c "netsh winsock reset catalog"
-    cmd.exe /c "netsh int ip reset reset.log"
+    cmd.exe /c "ipconfig /release"
     cmd.exe /c "ipconfig /flushdns"
-    cmd.exe /c "ipconfig /registerdns"
+    cmd.exe /c "ipconfig /renew"
+    cmd.exe /c "netsh int ip reset"
+    cmd.exe /c "netsh winsock reset"
     cmd.exe /c "route /f"
+    cmd.exe /c "netcfg -d"
     cmd.exe /c "sc config FDResPub start=auto"
     cmd.exe /c "sc config fdPHost start=auto"
-    cmd.exe /c "shutdown -r -t 70"
+    cmd.exe /c "shutdown -r -t 10"
 }
 
 Do-Next
