70ce40eabb9b12bae6a177b7dbed84e771d373e4
Anduin Xue <xuef@microsoft.com>
2023-04-01 20:11:50 +0000

    Refactor aria2 installation script
    - Automatically fetch the latest aria2 release
    - Use Expand-Archive instead of relying on 7-Zip for extraction
    - Store temporary files in the $env:TEMP folder instead of $HOME
    - Fix issues with $subPath to correctly move files from the subdirectory

--- install.ps1
+++ install.ps1
@@ -257,20 +257,34 @@ $fdmDbPath = "$env:LOCALAPPDATA\Softdeluxe\Free Download Manager\db.sqlite"
 Invoke-WebRequest -Uri "https://git.aiursoft.cn/Anduin/configuration-script-win/raw/branch/main/db.sqlite" -OutFile "$fdmDbPath"
 
 #aria2
-if ($true)
-{
+if ($true) {
     Write-Host "Installing aria2 as download tool..." -ForegroundColor Green
-    $downloadAddress = "https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-64bit-build1.zip"
-    Invoke-WebRequest $downloadAddress -OutFile "$HOME\aria2.zip"
+
+    $apiUrl = "https://api.github.com/repos/aria2/aria2/releases/latest"
+    $downloadAddress = (Invoke-RestMethod -Uri $apiUrl).assets |
+        Where-Object { $_.name -like "aria2-*-win-64bit-build1.zip" } |
+        Select-Object -ExpandProperty browser_download_url
+
+    Write-Host "Downloading aria2 from: $downloadAddress" -ForegroundColor Yellow
+
+    $aria2ZipPath = Join-Path -Path $env:TEMP -ChildPath "aria2.zip"
+    Invoke-WebRequest $downloadAddress -OutFile $aria2ZipPath
+
     $installPath = "${env:ProgramFiles}\aria2"
-    & "${env:ProgramFiles}\7-Zip\7z.exe" x "$HOME\aria2.zip" "-o$($installPath)" -y
-    $subPath = $(Get-ChildItem -Path $installPath | Where-Object { $_.Name -like "aria2-*" } | Sort-Object Name -Descending | Select-Object -First 1).Name
-    $subPath = Join-Path -Path $installPath -ChildPath $subPath
-    Remove-Item $installPath\aria2c.exe -ErrorAction SilentlyContinue
-    Move-Item $subPath\aria2c.exe $installPath
+    New-Item -ItemType Directory -Force -Path $installPath | Out-Null
+
+    Expand-Archive -LiteralPath $aria2ZipPath -DestinationPath $installPath -Force
+
+    $subPath = Get-ChildItem -Path $installPath | Where-Object { $_.Name -like "aria2-*" } | Sort-Object Name -Descending | Select-Object -First 1
+    $fullSubPath = Join-Path -Path $installPath -ChildPath $subPath.Name
+    Move-Item -Path "$fullSubPath\*" -Destination $installPath -Force
+    Remove-Item -Path $fullSubPath -Recurse -Force
+
     AddToPath -folder $installPath
-    Remove-Item -Path "$HOME\aria2.zip" -Force
+    Remove-Item -Path $aria2ZipPath -Force
 }
+
+
 
 #iperf3
 if ($true)
