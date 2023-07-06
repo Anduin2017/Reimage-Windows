02ad9cd6646bfa486357be509a2e5c27f269d242
Anduin Xue <xuef@microsoft.com>
2023-07-06 07:09:53 +0000

    Use cmd.

--- actions/ResetNet.psm1
+++ actions/ResetNet.psm1
@@ -1,15 +1,15 @@
 function ResetNet {
     Write-Host "Reseting Network...(ETA: 20 seconds...)" -ForegroundColor Green
-    ipconfig /release
-    ipconfig /flushdns
-    ipconfig /renew
-    netsh int ip reset
-    netsh winsock reset
-    route /f
-    netcfg -d
-    sc config FDResPub start=auto
-    sc config fdPHost start=auto
-    shutdown -r -t 10
+    cmd.exe /c "ipconfig /release"
+    cmd.exe /c "ipconfig /flushdns"
+    cmd.exe /c "ipconfig /renew"
+    cmd.exe /c "netsh int ip reset"
+    cmd.exe /c "netsh winsock reset"
+    cmd.exe /c "route /f"
+    cmd.exe /c "netcfg -d"
+    cmd.exe /c "sc config FDResPub start=auto"
+    cmd.exe /c "sc config fdPHost start=auto"
+    cmd.exe /c "shutdown -r -t 10"
     Start-Sleep -Seconds 3
 }
 
