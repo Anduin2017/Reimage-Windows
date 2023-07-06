e512f964bde0f95e9ef4a1da99788d8be6be7126
Anduin Xue <anduin@aiursoft.com>
2023-07-06 14:52:46 +0000

    Add gsudo.

--- components/InstallGSudo.psm1
+++ components/InstallGSudo.psm1
@@ -3,6 +3,8 @@ Import-Module "..\tools\AddToPath.psm1"
 
 function InstallGSudo {
     Install-IfNotInstalled "gerardog.gsudo"
+
+    AddToPath "$env:ProgramFiles\gsudo\Current\"
 }
 
 Export-ModuleMember -Function InstallGSudo
