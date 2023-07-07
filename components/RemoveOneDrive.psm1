7369030dae202a4b9535f62598476dbc6fe7c0fa
Anduin Xue <anduin@aiursoft.com>
2023-07-07 19:47:17 +0000

    Remove teams and onedrive.

--- components/RemoveOneDrive.psm1
+++ components/RemoveOneDrive.psm1
@@ -2,11 +2,9 @@ Import-Module "..\tools\RemoveUwp.psm1"
 
 function RemoveOneDrive {
     winget remove "Microsoft.OneDrive"
-    Get-Process -Name Teams -ErrorAction SilentlyContinue | Stop-Process
-    Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$env:ProgramFiles(x86)\Teams Installer" -Recurse -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$env:APPDATA\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$env:ProgramData\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
+    Get-Process -Name OneDrive -ErrorAction SilentlyContinue | Stop-Process
+    Remove-Item -Path "$env:ProgramFiles(x86)\Microsoft OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
 }
 
 Export-ModuleMember -Function RemoveOneDrive
--- components/RemoveTeams.psm1
+++ components/RemoveTeams.psm1
@@ -6,9 +6,12 @@ function RemoveTeams {
     winget remove "Teams Machine-Wide Installer"
     Get-Process -Name Teams -ErrorAction SilentlyContinue | Stop-Process
     Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\TeamsMeetingAddin" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\TeamsPresenceAddin" -Recurse -Force -ErrorAction SilentlyContinue
     Remove-Item -Path "$env:ProgramFiles(x86)\Teams Installer" -Recurse -Force -ErrorAction SilentlyContinue
     Remove-Item -Path "$env:APPDATA\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
     Remove-Item -Path "$env:ProgramData\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
+
 }
 
 Export-ModuleMember -Function RemoveTeams
