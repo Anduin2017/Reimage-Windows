03b0d8ce6f80abef97b8cb25d3202c6407994d12
Anduin Xue <anduin@aiursoft.com>
2023-07-07 19:42:45 +0000

    Remove teams and onedrive.

---
+++ components/InstallVS.psm1
@@ -0,0 +1,18 @@
+Import-Module "..\tools\Install-IfNotInstalled.psm1"
+
+function InstallVS {
+    param(
+        [string]$mail
+    )
+
+    if ($mail.Contains('microsoft')) {
+        Install-IfNotInstalled Microsoft.VisualStudio.2022.Enterprise
+    }
+    else {
+        Install-IfNotInstalled Microsoft.VisualStudio.2022.Community
+        winget remove Microsoft.OneDrive
+    }
+    
+}
+
+Export-ModuleMember -Function InstallVS
