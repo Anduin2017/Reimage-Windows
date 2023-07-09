09f0e094e439b4acecaae11af9663d0b47758e31
Anduin Xue <anduin@aiursoft.com>
2023-07-09 08:33:03 +0000

    New way to install profile.

--- actions/Setup-PowerShellEnvironment.psm1
+++ actions/Setup-PowerShellEnvironment.psm1
@@ -16,12 +16,7 @@ function Setup-PowerShellEnvironment {
     New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
 
     Write-Host "Installing profile file..." -ForegroundColor Green
-    if (!(Test-Path $PROFILE)) {
-        Write-Host "Creating PROFILE..." -ForegroundColor Yellow
-        New-Item -Path $PROFILE -ItemType "file" -Force
-    }
-    $profileContent = (New-Object System.Net.WebClient).DownloadString('https://gitlab.aiursoft.cn/anduin/reimage-windows/-/raw/master/PROFILE.ps1')
-    Set-Content $PROFILE $profileContent
+    Copy-Item -Path "$env:TEMP\reimage-windows-master\PROFILE.ps1" -Destination $PROFILE
     . $PROFILE
 }
 
