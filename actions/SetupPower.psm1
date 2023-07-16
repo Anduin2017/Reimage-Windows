8a6dfa9e0d112392ac10d45991e1e05265261e21
AnduinXue <anduin@aiursoft.com>
2023-07-16 07:49:54 +0000

    Hybernate set to on.

--- actions/SetupPower.psm1
+++ actions/SetupPower.psm1
@@ -7,6 +7,14 @@ function SetupPower {
     Powercfg /Change monitor-timeout-ac 20
     Powercfg /Change standby-timeout-ac 0
     Write-Host "Monitor timeout set to 20."
+
+    Write-Host "Enabling Hibernate..." -ForegroundColor Green
+    powercfg /hibernate on
+    Write-Host "Hibernate enabled."
+
+    Write-Host "Setting hibernate mode to full..." -ForegroundColor Green
+    powercfg /hibernate /type full
+    Write-Host "Hibernate mode set to full."
 }
 
 Export-ModuleMember -Function SetupPower
