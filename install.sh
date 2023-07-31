3a720418ec4db07af3bb03587c03d86b3fefe177
Anduin Xue <anduin@aiursoft.com>
2023-07-31 17:55:48 +0000

    Add nextcloud source.

--- install.sh
+++ install.sh
@@ -34,6 +34,9 @@ rm -f packages.microsoft.gpg
 curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
+# Nextcloud
+sudo add-apt-repository ppa:nextcloud-devs/client --yes
+
 # Snap
 sudo snap remove firefox
 sudo snap remove snap-store
