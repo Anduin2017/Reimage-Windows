1d809467448aae2d7d2940b5d80f5b6f6d86df25
Anduin Xue <anduin@aiursoft.com>
2023-09-30 16:37:51 +0000

    Install sdk 7 first.

--- components/InstallDotnet.psm1
+++ components/InstallDotnet.psm1
@@ -28,8 +28,8 @@ function TryInstallDotnetTool {
 }
 
 function InstallDotnet {
-  Install-IfNotInstalled "Microsoft.DotNet.SDK.6"
   Install-IfNotInstalled "Microsoft.DotNet.SDK.7"
+  Install-IfNotInstalled "Microsoft.DotNet.SDK.6"
   Install-IfNotInstalled "Microsoft.NuGet"
 
   Write-Host "Setting up .NET environment variables..." -ForegroundColor Green
