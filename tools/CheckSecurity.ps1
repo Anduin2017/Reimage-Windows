d362f91aab564884617633a800c3ef3b8a8eed33
xuef <xuef@microsoft.com>
2023-11-08 09:33:14 +0000

    Add readme.

--- tools/CheckSecurity.ps1
+++ tools/CheckSecurity.ps1
@@ -1,3 +1,12 @@
+function Get-IsElevated {
+    $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $p = New-Object System.Security.Principal.WindowsPrincipal($id)
+    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
+    { Write-Output $true }      
+    else
+    { Write-Output $false }   
+}
+
 function EnsureElevated {
     if (-not(Get-IsElevated)) { 
         throw "Please run this script as an administrator" 
