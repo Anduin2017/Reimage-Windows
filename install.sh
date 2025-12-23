725cdfbcb69d8914ff334f320b86341844254dd1
Anduin Xue <anduin@aiursoft.com>
2025-12-23 09:28:24 +0000

    Node majro to 24.

--- install.sh
+++ install.sh
@@ -107,7 +107,7 @@ flatpak install -y flathub io.bassi.Amberol
 
 # Node
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg --yes
-NODE_MAJOR=22
+NODE_MAJOR=24
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install nodejs -y
